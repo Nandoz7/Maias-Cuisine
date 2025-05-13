ff906dcf74420b5e114ab4176482bd9d7f9dfb95
Nandoz7 <luis.augusto@sptech.school>
2025-05-12 21:19:54 -0300

    Finalizando css de home, login e cadastro; Mexendo com a API para postagens de receitas

---
+++ src/controllers/receitaController.js
@@ -0,0 +1,137 @@
+var receitaModel = require("../models/receitaModel");
+
+function listar(req, res) {
+    receitaModel.listar().then(function (resultado) {
+        if (resultado.length > 0) {
+            res.status(200).json(resultado);
+        } else {
+            res.status(204).send("Nenhum resultado encontrado!")
+        }
+    }).catch(function (erro) {
+        console.log(erro);
+        console.log("Houve um erro ao buscar as receitas: ", erro.sqlMessage);
+        res.status(500).json(erro.sqlMessage);
+    });
+}
+
+function listarPorUsuario(req, res) {
+    var idUsuario = req.params.idUsuario;
+
+    receitaModel.listarPorUsuario(idUsuario)
+        .then(
+            function (resultado) {
+                if (resultado.length > 0) {
+                    res.status(200).json(resultado);
+                } else {
+                    res.status(204).send("Nenhum resultado encontrado!");
+                }
+            }
+        )
+        .catch(
+            function (erro) {
+                console.log(erro);
+                console.log(
+                    "Houve um erro ao buscar os avisos: ",
+                    erro.sqlMessage
+                );
+                res.status(500).json(erro.sqlMessage);
+            }
+        );
+}
+
+function pesquisarReceita(req, res) {
+    var receita = req.params.receita;
+
+    receitaModel.pesquisarReceita(receita)
+        .then(
+            function (resultado) {
+                if (resultado.length > 0) {
+                    res.status(200).json(resultado);
+                } else {
+                    res.status(204).send("Nenhum resultado encontrado!");
+                }
+            }
+        ).catch(
+            function (erro) {
+                console.log(erro);
+                console.log("Houve um erro ao buscar as receitas: ", erro.sqlMessage);
+                res.status(500).json(erro.sqlMessage);
+            }
+        );
+}
+
+function publicar(req, res) {
+    var titulo = req.body.titulo;
+    var pqGosta = req.body.pqGosta;
+    var comoFazer = req.body.comoFazer;
+    var idUsuario = req.params.idUsuario;
+
+    if (titulo == undefined) {
+        res.status(400).send("O título está indefinido!");
+    } else if (descricao == undefined) {
+        res.status(400).send("A descrição está indefinido!");
+    } else if (idUsuario == undefined) {
+        res.status(403).send("O id do usuário está indefinido!");
+    } else {
+        avisoModel.publicar(titulo, descricao, idUsuario)
+            .then(
+                function (resultado) {
+                    res.json(resultado);
+                }
+            )
+            .catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log("Houve um erro ao realizar o post: ", erro.sqlMessage);
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+    }
+}
+
+function editar(req, res) {
+    var novaEdicao = req.body.descricao;
+    var idReceita = req.params.idAviso;
+
+    avisoModel.editar(novaEdicao, idReceita)
+        .then(
+            function (resultado) {
+                res.json(resultado);
+            }
+        )
+        .catch(
+            function (erro) {
+                console.log(erro);
+                console.log("Houve um erro ao realizar o post: ", erro.sqlMessage);
+                res.status(500).json(erro.sqlMessage);
+            }
+        );
+
+}
+
+function deletar(req, res) {
+    var idReceita = req.params.idReceita;
+
+    receitaModel.deletar(idReceita)
+        .then(
+            function (resultado) {
+                res.json(resultado);
+            }
+        )
+        .catch(
+            function (erro) {
+                console.log(erro);
+                console.log("Houve um erro ao deletar o post: ", erro.sqlMessage);
+                res.status(500).json(erro.sqlMessage);
+            }
+        );
+}
+
+module.exports = {
+    listar,
+    listarPorUsuario,
+    pesquisarReceita,
+    publicar,
+    editar,
+    deletar
+}
